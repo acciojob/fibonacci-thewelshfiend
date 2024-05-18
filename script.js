13ff75df1335d14c905ad937bfd1838434f498ec
Kiran TJ <166934975+thewelshfiend@users.noreply.github.com>
2024-05-18 09:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function fibonacci(num) {
+function fibonacci(num, i = 1, a = 0, b = 1) {
 // your code here
+	if (i == num)
+	{
+		return a;
+	}
+	const temp = a + b;
+	return fibonacci(num, i + 1, b, temp);
 }
 
 module.exports = fibonacci;
